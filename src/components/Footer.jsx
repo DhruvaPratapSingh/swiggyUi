0934a66c6c1f0f8e6158180627105a159cff6c28
dhruvathakur <128789248+DhruvaPratapSingh@users.noreply.github.com>
2025-01-26 16:44:01 +0530

    document formated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 
 const Footer = () => {
   return (
-    <div className="flex justify-center items-center font-extrabold text-[200px]">
+    <div className="flex justify-center items-center font-medium text-[100%]">
       footer
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,78 +7,98 @@ import { IoIosHelpBuoy } from "react-icons/io";
 import { FaRegUser } from "react-icons/fa";
 import { CiShoppingCart } from "react-icons/ci";
 import LeafletMapSearch from "./Map";
-const Header = () => {
-    const [toggle,settoggle]=useState(false);
-    const toggleleft=()=>{
-     settoggle(true);
-    }
-    const hidetoggle=()=>{
-     settoggle(false);
-    }
-    const links=[
-        {
-            icon:<MdOutlineDesktopWindows />,
-            name:"Swiggy corporate"
-        },
-        {
-            icon:<CiSearch />,
-            name:"Search"
-        },
-        {
-            icon:<BiSolidOffer />,
-            name:"Offers",
-            sup:"New"
-        },
-        {
-            icon:<IoIosHelpBuoy />            ,
-            name:"Help"
-        },
-        {
-            icon:<FaRegUser />,
-            name:"Sign In"
-        },
-        {
-            icon:<CiShoppingCart/>,
-            name:"Cart"
-        },
-    ]
-    
-  return (
-   <>
-    <div onClick={hidetoggle} className="black-overlay w-full h-full fixed duration-500 " style={{opacity: toggle ? 1 : 0,
-    visibility: toggle ? "visible" : "hidden"}}>
-    </div>
-    <div className="hidden md:z-[9999] cursor-pointer w-[500px] bg-white h-full absolute duration-[600ms]"
-    style={{
-        left:toggle ? "0%" : "-100%"
-    }}>
-    <div className="p-5 mt-14 flex justify-center items-center ">
-   <LeafletMapSearch/>
-    </div>
-    </div>
-     <header className='p-3 shadow-xl sticky top-0 z-[999]' style={{backgroundColor: toggle ? "rgb(0, 0, 0,0.7)" : "white"}}>
-      <div className='max-w-[1200px] mx-auto flex items-center'>
-        <div className='w-[100px]'>
-          <img className="w-full" src='/slogo.png' alt=''/>
-        </div>
-        <div onClick={toggleleft} className="hover:text-[#ff5200] ">
-          <span className="border-b-2 border-gray-600 cursor-pointer  font-medium hover:border-[#ff5200]">others </span>
-          <RiArrowDropDownLine className="inline text-[#ff5200] text-[40px] cursor-pointer"/>
-        </div>
-      <nav className="hidden md:flex list-none gap-10  ml-auto text-[18px]">
-       {
-        links.map((item,idx)=>{
-            return <li key={idx} className=" flex items-center gap-2 cursor-pointer hover:text-[#ff5200]">
-            {item.icon}
-            <span>{item.name} <sup className="text-[#ee9d77]">{item.sup}</sup> </span>
-            </li>
-        })
-       }
-      </nav>
-      </div>
-    </header>
-   </>
-  )
-}
 
-export default Header
+const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const toggleLeft = () => {
+    setToggle(true);
+  };
+
+  const hideToggle = () => {
+    setToggle(false);
+  };
+
+  const links = [
+    {
+      icon: <MdOutlineDesktopWindows />,
+      name: "Swiggy corporate",
+    },
+    {
+      icon: <CiSearch />,
+      name: "Search",
+    },
+    {
+      icon: <BiSolidOffer />,
+      name: "Offers",
+      sup: "New",
+    },
+    {
+      icon: <IoIosHelpBuoy />,
+      name: "Help",
+    },
+    {
+      icon: <FaRegUser />,
+      name: "Sign In",
+    },
+    {
+      icon: <CiShoppingCart />,
+      name: "Cart",
+    },
+  ];
+
+  return (
+    <>
+      <div
+        onClick={hideToggle}
+        className={`black-overlay w-full h-full fixed duration-500 bg-black/50 ${
+          toggle ? "opacity-100 visible" : "opacity-0 invisible"
+        }`}
+      ></div>
+      <div
+        className={`z-[9999] cursor-pointer w-[75%] sm:w-[500px] bg-white h-full fixed top-0 left-0 duration-500 transform ${
+          toggle ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <div className="p-2 mt-14 flex justify-center items-center w-[50%] md:w-[100%]">
+          <LeafletMapSearch />
+        </div>
+      </div>
+      <header
+        className={`p-3 shadow-xl sticky top-0 z-[999] duration-300 ${
+          toggle ? "bg-black/70" : "bg-white"
+        }`}
+      >
+        <div className="max-w-[1200px] mx-auto flex items-center">
+          <div className="w-[100px]">
+            <img className="w-full" src="/slogo.png" alt="Logo" />
+          </div>
+          <div
+            onClick={toggleLeft}
+            className="hover:text-[#ff5200] cursor-pointer ml-4"
+          >
+            <span className="border-b-2 border-gray-600 font-medium hover:border-[#ff5200]">
+              Others
+            </span>
+            <RiArrowDropDownLine className="hidden md:inline text-[#ff5200] text-[40px]" />
+          </div>
+          <nav className="hidden md:flex list-none gap-10 ml-auto text-[18px]">
+            {links.map((item, idx) => (
+              <li
+                key={idx}
+                className="flex items-center gap-2 cursor-pointer hover:text-[#ff5200]"
+              >
+                {item.icon}
+                <span>
+                  {item.name} <sup className="text-[#ee9d77]">{item.sup}</sup>
+                </span>
+              </li>
+            ))}
+          </nav>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
